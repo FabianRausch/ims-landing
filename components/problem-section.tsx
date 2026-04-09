8b326c5fca78d7dee1e2ed4c563ccbd52f6a4c2a
FabianRausch <fabianr95.fr@gmail.com>
2026-04-09 00:24:55 -0300

    Add images to ProblemSection component and fix heading class

--- components/problem-section.tsx
+++ components/problem-section.tsx
@@ -4,7 +4,7 @@ export function ProblemSection() {
       <div className="container mx-auto p-4">
         <div className="max-w-3xl mx-auto text-center p-6  bg-white rounded-xl shadow-xl">
           <div className="flex justify-center items-center rounded-full p-5 mb-6">
-            <h2 className="text-3xl md:text-4xl font-bold text-balance text-">
+            <h2 className="text-3xl md:text-4xl font-bold text-balance ">
               ¿Te gustaría mejorar tu presencia en Online?
             </h2>
           </div>
@@ -16,6 +16,15 @@ export function ProblemSection() {
             enfocadas en mostrar tu negocio al público adecuado y mejorar tu
             presencia online.
           </p>
+          <div className="flex items-center justify-between my-6">
+            <img src="/funnel.png" alt="Funnel" className="w-25 h-25 mx-auto" />
+            <img
+              src="/audience.png"
+              alt="Audience"
+              className="w-25 h-25 mx-auto"
+            />
+            <img src="/growth.png" alt="Growth" className="w-25 h-25 mx-auto" />
+          </div>
         </div>
       </div>
     </section>
